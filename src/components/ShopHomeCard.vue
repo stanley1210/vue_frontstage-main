c60763f60adbfee27c4fdd2eeb06bf1b4fb6ebe2
stanley1210 <stbs1210@gmail.com>
2024-07-27 17:42:02 +0800

    123

--- src/components/ShopHomeCard.vue
+++ src/components/ShopHomeCard.vue
@@ -1,32 +1,45 @@
 <template>
         <div class="card" style="width: 30rem;">
-                <img :src="`${path}${shopHomeCard.id}`" class="card-img-top">
-                <div class="card-body navbarBody">
-                        <el-icon :size="16" class="likeLogo" @click="handleLikeCreate">
-                                <Star />
-                        </el-icon>
-                        <h5 class="card-title">ID.{{ shopHomeCard.id }}</h5>
-                        <p class="card-text">{{ shopHomeCard.carinfoModelName }}</p>
-                        <div class="d-flex flex-row ">
-                                <div class="card-text p-2 flex-grow-1">(里程){{ shopHomeCard.milage }}/ (年份){{
-                                        shopHomeCard.productionYear }}</div>
-                                <div class="card-text p-2">{{ shopHomeCard.price }} NTD</div>
-                        </div>
-                        <a href="#" class="btn btn-primary">我真的需要這個酷東西！！</a>
+            <img :src="`${path}${shopHomeCard.id}`" class="card-img-top">
+            <div class="card-body navbarBody">
+                <el-icon :size="16" class="likeLogo" @click="handleLikeCreate">
+                    <Star />
+                </el-icon>
+                <h5 class="card-title">ID.{{ shopHomeCard.id }}</h5>
+                <p class="card-text">{{ shopHomeCard.carinfoModelName }}</p>
+                <div class="d-flex flex-row ">
+                    <div class="card-text p-2 flex-grow-1">
+                        (里程){{ shopHomeCard.milage }} / (年份){{ shopHomeCard.productionYear }}
+                    </div>
+                    <div class="card-text p-2">{{ shopHomeCard.price }} NTD</div>
                 </div>
+                <a href="#" class="btn btn-primary" @click="navigateToCarDetail">我真的需要這個酷東西！！</a>
+            </div>
         </div>
-</template>
+    </template>
+    
+    <script setup>
+    import { useRouter } from 'vue-router'; // 导入 useRouter
 
-<script setup>
-const props = defineProps(["shopHomeCard"]);
-const path = import.meta.env.VITE_PHOTO;
+    const props = defineProps(["shopHomeCard"]);
+    const path = import.meta.env.VITE_PHOTO;
+    const router = useRouter(); // 创建 useRouter 实例
 
-//需要car.id customer.id
-const emit = defineEmits(["likeCreate"]);
-
-function handleLikeCreate() {
-    emit('likeCreate', props.shopHomeCard.id);
-}
-</script>
+    const emit = defineEmits(["likeCreate"]);
+    console.log("paramsData"+paramsData)
+    function handleLikeCreate() {
+        emit('likeCreate', props.shopHomeCard.id);
+    }
+    
+    function navigateToCarDetail(event) {
+        event.preventDefault(); // 阻止默认的 <a> 标签跳转行为
+        if (props.shopHomeCard.id) {
+            router.push({ name: 'pages-shop-car-link', params: { id: props.shopHomeCard.id } });
+        } else {
+            console.error("Invalid shopHomeCard id:", props.shopHomeCard.id);
+        }
+    }
+    </script>
+    
 
 <style></style>
