ba0e2f60f9fe26e15253fa667a35a4bc52ff3432
stanley1210 <stbs1210@gmail.com>
2024-07-27 18:13:51 +0800

    123

--- src/views/pages/shop/Car.vue
+++ src/views/pages/shop/Car.vue
@@ -3,35 +3,7 @@
     <!-- ------------------------------------------大圖------------------------------------------ -->
     <div id="carouselExampleIndicators" class="carousel slide navbarBody" data-bs-ride="carousel"
         data-bs-interval="4000">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="../../../../public/supra1.png" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../../public/supra2.jpeg" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="../../../../public/supra3.jpeg" class="d-block w-100">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+        <!-- Carousel 内容 -->
     </div>
 
     <!-- ------------------------------------------資料行 ------------------------------------------ -->
@@ -66,14 +38,10 @@ import Swal from 'sweetalert2';
 import { ref, computed, onMounted, watch } from 'vue';
 import CarColumnL from '@/components/CarColumnL.vue';
 import CarColumnR from '@/components/CarColumnR.vue';
-
 import { useRoute } from 'vue-router'; // 新增导入 useRoute
+
 const route = useRoute(); // 新增 useRoute 实例
 const carId = ref(route.params.id); // 新增获取路由参数
-const router =useRouter();
-const paramsData=route.params;
-console.log("route.params.id"+paramsData.id)
-console.log("carId.value"+carId.value)
 
 // 串接登入會員,這邊下面的import一定要加
 import { useStore } from 'vuex';
@@ -88,17 +56,15 @@ onMounted(() => {
 customerInfo = computed(() => store.state.customerInfo.data || {});
 console.log('===>test Customer info:', customerInfo);
 
-
-
-
 const carDatas = ref([]); // 資料列表
 const selectedCarId = ref(null); // 当前选择的汽车 ID
 
 //搜尋單筆car資訊
-axios.get(`http://localhost:8080/kajarta/car/find/${carId.value}`)
+
+axios.get(`http://localhost:8080/kajarta/car/find/${carId.value}`) // 修改请求 URL，使用 carId.value
     .then(function (response) {
-        console.log("API Response:", response); // 输出 API 返回的数据
         if (response && response.data) {
+            console.log("response", response);
             carDatas.value = response.data.list;
             if (carDatas.value.length > 0) {
                 selectedCarId.value = carDatas.value[0].id; // 假设你选择了第一个汽车
@@ -107,9 +73,13 @@ axios.get(`http://localhost:8080/kajarta/car/find/${carId.value}`)
         } else {
             console.error("Invalid response data structure:", response);
         }
+
+        // setTimeout(function () {
+        //     Swal.close();
+        // }, 500);
     })
     .catch(function (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data:", error, response);
         Swal.fire({
             text: "查詢失敗：" + error.message,
             icon: "error"
