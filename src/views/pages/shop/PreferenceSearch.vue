473504be74d2fc751ba6dab9743e834dd2e090ef
nick <gh3342002@hotmail.com.tw>
2024-08-08 13:03:04 +0800

    111

--- src/views/pages/shop/PreferenceSearch.vue
+++ src/views/pages/shop/PreferenceSearch.vue
@@ -1,84 +1,119 @@
 <template>
   <section>
-  <Navigation v-if="props.showNavigation"></Navigation>
-  <br />
-  <!-- 顯示查詢結果 -->
-  <h3 style="color: #a33238; font-weight: bold;">車輛查詢結果</h3>
-  <br>
-  <div v-if="paginatedResults.length > 0" class="card-container" >
-    <div class="card" v-for="data in paginatedResults" :key="data.id" :data="data">
-    <img class="card-img-top" :src="photoSrc(data)" :alt="data.modelName" :id="data.id"> 
-    <h5 class="card-title" style="display: none;">ID.{{ data.id }}</h5>
-      <div class="card-body navbarBody" style="border: unset;">
-        <h5 class="card-title" style="padding-left: 0; color: #a33238; font-weight: bold; padding-right: 0;">{{ data.modelName }}</h5>
-        <p class="card-text" style="padding-left: 0;  color: #a33238;  font-weight: bold; padding-right: 0;">
-          年分: {{ data.productionYear }}<br />
-          價格: {{ data.price }} <br />
-          里程數: {{ data.milage }} <br />
-          車況評分: {{ data.conditionScore }} <br />
-          馬力: {{ data.hp }} <br />
-          扭力: {{ data.torque }} <br />
-          品牌: {{ data.brand }} <br />
-          車型: {{ data.suspension }} <br />
-          車門數: {{ data.door }} <br />
-          乘客數: {{ data.passenger }} <br />
-          驅動方式: {{ data.rearWheel }} <br />
-          引擎燃料: {{ data.gasoline }} <br />
-          變速系統: {{ data.transmission }} <br />
-          排氣量: {{ data.cc }} <br />
-        </p>
-        <el-button type="primary"  color="#a33238" @click="cardetail(data)">車輛詳細資訊</el-button>
+    <Navigation v-if="props.showNavigation"></Navigation>
+    <br />
+    <!-- 顯示查詢結果 -->
+    <h3 style="color: #a33238; font-weight: bold">車輛查詢結果</h3>
+    <br />
+    <div v-if="paginatedResults.length > 0" class="card-container">
+      <div
+        class="card"
+        v-for="data in paginatedResults"
+        :key="data.id"
+        :data="data"
+      >
+        <img
+          class="card-img-top"
+          :src="photoSrc(data)"
+          :alt="data.modelName"
+          :id="data.id"
+        />
+        <h5 class="card-title" style="display: none">ID.{{ data.id }}</h5>
+        <div class="card-body navbarBody" style="border: unset">
+          <h5
+            class="card-title"
+            style="
+              padding-left: 0;
+              color: #a33238;
+              font-weight: bold;
+              padding-right: 0;
+            "
+          >
+            {{ data.modelName }}
+          </h5>
+          <p
+            class="card-text"
+            style="
+              padding-left: 0;
+              color: #a33238;
+              font-weight: bold;
+              padding-right: 0;
+            "
+          >
+            年分: {{ data.productionYear }}<br />
+            價格: {{ data.price }} <br />
+            里程數: {{ data.milage }} <br />
+            車況評分: {{ data.conditionScore }} <br />
+            馬力: {{ data.hp }} <br />
+            扭力: {{ data.torque }} <br />
+            品牌: {{ data.brand }} <br />
+            車型: {{ data.suspension }} <br />
+            車門數: {{ data.door }} <br />
+            乘客數: {{ data.passenger }} <br />
+            驅動方式: {{ data.rearWheel }} <br />
+            引擎燃料: {{ data.gasoline }} <br />
+            變速系統: {{ data.transmission }} <br />
+            排氣量: {{ data.cc }} <br />
+          </p>
+          <el-button type="primary" color="#a33238" @click="cardetail(data)"
+            >車輛詳細資訊</el-button
+          >
+        </div>
       </div>
     </div>
-  </div>
-  <div v-else>
-    <br>
-  <p style="color: #a33238; font-weight: bold; ">未查詢到車輛</p>
-</div>
-<br>
-<!-- 分頁效果(上下頁) -->
-<div class="pagination-container">
-  <el-pagination
-      @current-change="handlePageChange"
-      :current-page="currentPage"
-      :page-size="pageSize"
-      :page-sizes="[2, 3, 4, 5]"
-      layout=" prev, pager, next, jumper"
-      :total="results.length">
-<!-- 自訂顯示幾筆 -->
-    </el-pagination>
-     <el-select v-model="pageSize" placeholder="每頁顯示數量" @change="handlePageSizeChange" style="width: 60px;">
-      <el-option label="2" :value="2"></el-option>
-      <el-option label="3" :value="3"></el-option>
-      <el-option label="4" :value="4"></el-option>
-      <el-option label="5" :value="5"></el-option>
-    </el-select>
-    <div>
-      每頁顯示 {{ pageSize }} 筆
+    <div v-else>
+      <br />
+      <p style="color: #a33238; font-weight: bold">未查詢到車輛</p>
     </div>
-</div>
-  <br>
-  <el-button @click="goBack" type="default" color="#a33238">返回查詢</el-button>
-</section>
+    <br />
+    <!-- 分頁效果(上下頁) -->
+    <div class="pagination-container">
+      <el-pagination
+        @current-change="handlePageChange"
+        :current-page="currentPage"
+        :page-size="pageSize"
+        :page-sizes="[2, 3, 4, 5]"
+        layout=" prev, pager, next, jumper"
+        :total="results.length"
+      >
+        <!-- 自訂顯示幾筆 -->
+      </el-pagination>
+      <el-select
+        v-model="pageSize"
+        placeholder="每頁顯示數量"
+        @change="handlePageSizeChange"
+        style="width: 60px"
+      >
+        <el-option label="2" :value="2"></el-option>
+        <el-option label="3" :value="3"></el-option>
+        <el-option label="4" :value="4"></el-option>
+        <el-option label="5" :value="5"></el-option>
+      </el-select>
+      <div>每頁顯示 {{ pageSize }} 筆</div>
+    </div>
+    <br />
+    <el-button @click="goBack" type="default" color="#a33238"
+      >返回查詢</el-button
+    >
+  </section>
 </template>
 
 <script setup>
-import { onMounted, ref,computed } from 'vue';
-import { useRoute,useRouter  } from 'vue-router';
-import Navigation from '@/views/Navigation.vue';
-import axiosapi from '@/plugins/axios';
+import { onMounted, ref, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import Navigation from "@/views/Navigation.vue";
+import axiosapi from "@/plugins/axios";
 
 const showNavigation = ref(true);
-const path =import.meta.env.VITE_PHOTO;
+const path = import.meta.env.VITE_PHOTO;
 const route = useRoute();
 const router = useRouter();
 const query = route.query;
 const results = ref([]);
 const currentPage = ref(1);
 const pageSize = ref(4);
-const isMainPicDatas=ref([]);
+const isMainPicDatas = ref([]);
 const kajartaUrl = import.meta.env.VITE_API_URL;
-
 
 // 分頁
 const paginatedResults = computed(() => {
@@ -87,24 +122,23 @@ const paginatedResults = computed(() => {
   return results.value.slice(start, end);
 });
 
-
 // 搜尋結果
 const handleSearchByNoMemSearch = async () => {
-  try { 
-    const response = await axiosapi.get('/preference/searchMore', {
+  try {
+    const response = await axiosapi.get("/preference/searchMore", {
       params: {
         carinfoId: query.carinfoId,
-        brand:  query.brand,
-        suspension:  query.suspension,
+        brand: query.brand,
+        suspension: query.suspension,
         door: query.door,
         passenger: query.passenger,
         rearwheel: query.rearwheel,
         gasoline: query.gasoline,
-        transmission:  query.transmission,
-        cc:  query.cc,
-        score:  query.score,
+        transmission: query.transmission,
+        cc: query.cc,
+        score: query.score,
         modelName: query.modelName || null,
-        productionYear: +query.productionYear || null,
+        productionYear: query.productionYear || null,
         price: query.price ? parseFloat(query.price) : null,
         milage: query.milage ? parseInt(query.milage) : null,
         hp: query.hp ? parseInt(query.hp) : null,
@@ -112,51 +146,47 @@ const handleSearchByNoMemSearch = async () => {
       },
     });
     results.value = response.data.preferenceCarList;
-    results.value.forEach(cars => {
-      axiosapi.get(`/image/isMainPic/${cars.id}`)
+    results.value.forEach((cars) => {
+      axiosapi
+        .get(`/image/isMainPic/${cars.id}`)
         .then(function (response) {
-            if (response && response.data) {
-              
-                isMainPicDatas.value = response.data.isMainPic;
-            
-            } else {
-
-            }          
+          if (response && response.data) {
+            isMainPicDatas.value = response.data.isMainPic;
+          } else {
+          }
         })
         .catch(function (error) {
-            console.error("Error fetching data:", error, response);
-            Swal.fire({
-                text: "查詢失敗：" + error.message,
-                icon: "error"
-            });
+          console.error("Error fetching data:", error, response);
+          Swal.fire({
+            text: "查詢失敗：" + error.message,
+            icon: "error",
+          });
         });
-
-    })
+    });
     // console.log('查詢結果:', response.data);
   } catch (error) {
-    console.error('查詢失敗:', error);
+    console.error("查詢失敗:", error);
   }
 };
 
-
 //抓圖片ID
-const photoSrc = async(data) =>{
+const photoSrc = async (data) => {
   // console.log("data.carId",data.id)
-try{
-const response = await axiosapi.get(`/image/isMainPic/${data.id}`);
-// console.log("response.data.isMainPic===========",response.data.isMainPic)
-document.getElementById(data.id).src = path+response.data.isMainPic;
-// console.log("data.carId",data.id)
-}catch(error){
-  console.error("error",error)
-}
+  try {
+    const response = await axiosapi.get(`/image/isMainPic/${data.id}`);
+    // console.log("response.data.isMainPic===========",response.data.isMainPic)
+    document.getElementById(data.id).src = path + response.data.isMainPic;
+    // console.log("data.carId",data.id)
+  } catch (error) {
+    console.error("error", error);
+  }
 };
 
 //搜尋結果導入車輛資訊
 const cardetail = (data) => {
   console.log("ID value:", data.id);
   console.log("ID type:", typeof data.id);
-  router.push({ name: 'pages-shop-car-link', query: { carId: data.id } });
+  router.push({ name: "pages-shop-car-link", query: { carId: data.id } });
 };
 
 // 分頁有上下頁選擇
@@ -173,8 +203,8 @@ const handlePageSizeChange = (newSize) => {
 // 返回搜尋頁面
 const goBack = () => {
   router.push({
-    name: 'pages-shop-home-link'
-})
+    name: "pages-shop-home-link",
+  });
 };
 
 onMounted(() => {
@@ -187,13 +217,12 @@ onMounted(() => {
 const props = defineProps({
   showNavigation: {
     type: Boolean,
-    default: true
-  }
+    default: true,
+  },
 });
 </script>
 
 <style scoped>
-
 /* 結果並排顯示 */
 .card-container {
   margin: 0;
@@ -214,8 +243,7 @@ const props = defineProps({
 .pagination-container {
   display: flex;
   align-items: center;
-  gap: 16px; 
-
+  gap: 16px;
 }
 
 .card-body {
@@ -233,8 +261,4 @@ const props = defineProps({
   font-size: 1rem;
   line-height: 1.5;
 }
-
-
-
-
 </style>
